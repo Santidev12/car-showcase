090db34b78ddb7f6397da8b0d1d0ea02168db8c4
Santiago Pulido Diaz <santiagopulido12@gmail.com>
2023-09-06 18:59:16 +0200

    second commit

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    reactStrictMode: true,
+
+ // Add basePath
+  basePath: '/github-pages',
+    
     images: {
         domains: ['cdn.imagin.studio']
     }
