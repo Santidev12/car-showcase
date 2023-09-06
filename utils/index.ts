8b8a4b14835c71ce348fb83d961edd4f08d06690
Santiago Pulido Diaz <santiagopulido12@gmail.com>
2023-09-06 18:18:28 +0200

    second commit

--- utils/index.ts
+++ utils/index.ts
@@ -43,7 +43,7 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
     const url = new URL("https://cdn.imagin.studio/getimage");
     const { make, model, year } = car;
   
-    url.searchParams.append('customer', process.env.KEY || "");
+    url.searchParams.append('customer', process.env.KEY || '');
     url.searchParams.append('make', make);
     url.searchParams.append('modelFamily', model.split(" ")[0]);
     url.searchParams.append('zoomType', 'fullscreen');
